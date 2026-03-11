faca5cfaa0c5f96755c882cf847f597dc95eb160
Ara <ara.public@pm.me>
2026-03-11 22:20:56 +0900

    refactor: 브라우저 확장에서 유저 스크립트로 전환
    - 더 이상 사용하지 않는 브라우저 확장 프로그램 관련 파일 삭제

--- userscript.js
+++ userscript.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name        Pixiv Redirector
+// @namespace   Violentmonkey Scripts
+// @match       *://www.pixiv.net/*
+// @grant       none
+// @version     1.0
+// @author      ara-hwang
+// @description 2026. 3. 11. 오후 9:38:31
+// @run-at      document-start
+// ==/UserScript==
+
 (() => {
   try {
     const params = new URLSearchParams(window.location.search);
